d0f7c38a469afdd919c75a77f91834c97ca28c8b
_ms93 <micol.spitale@gmail.com>
2019-06-26 17:53:22 +0200

    Add files via upload

--- player.js
+++ player.js
@@ -152,7 +152,7 @@ $(document).ready(function () {
             callback(stream);
         });
     }
-   
+
 });
 
 
@@ -166,4 +166,69 @@ function handleReceiveMessage(event) {
     console.log("Ricevuto:" + event.data);
     //var res = event.data.split(",");
     //animation(res[0], res[1]);
+}
+
+
+function listeningAnimator() {
+    var waves = new SineWaves({
+        el: document.getElementById('waves'),
+        speed: 2,
+        ease: 'SineInOut',
+        wavesWidth: '75%',
+        waves: [
+            {
+                timeModifier: 4,
+                lineWidth: 1,
+                amplitude: -25,
+                wavelength: 25
+    },
+            {
+                timeModifier: 2,
+                lineWidth: 1,
+                amplitude: -10,
+                wavelength: 30
+    },
+            {
+                timeModifier: 1,
+                lineWidth: 1,
+                amplitude: -30,
+                wavelength: 30
+    },
+            {
+                timeModifier: 3,
+                lineWidth: 1,
+                amplitude: 40,
+                wavelength: 40
+    },
+            {
+                timeModifier: 0.5,
+                lineWidth: 1,
+                amplitude: -60,
+                wavelength: 60
+    },
+            {
+                timeModifier: 1.3,
+                lineWidth: 1,
+                amplitude: -40,
+                wavelength: 40
+    }
+  ],
+
+        resizeEvent: function () {
+            var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
+            gradient.addColorStop(0, "rgba(255,221,116,0.49)");
+            gradient.addColorStop(0.5, "rgba(255,221,116,0.49)");
+            gradient.addColorStop(1, "rgba(255, 255, 25, 0");
+
+            var index = -1;
+            var length = this.waves.length;
+            while (++index < length) {
+                this.waves[index].strokeStyle = gradient;
+            }
+
+            index = void 0;
+            length = void 0;
+            gradient = void 0;
+        }
+    });
 }
